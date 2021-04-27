11f76301618980f8d2e82e3ae3e34d0df9697043
JaehyeonHeo <jhheo06@gmail.com>
2021-04-27 10:46:04 +0900

    Commit

---
+++ 2-1.c/2-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // 헤더파일이며 확장자는 .h => standard input output(표준입출력)
+
+int main(void)		// 메인함수 
+{						// 메인함수 시작 
+	printf("Be happy\n");  // 콘솔창에 출력하는 표준 출력함수 
+	printf("My friend\n");  // "\n"은 한줄바꾸기(엔터)
+
+	return 0;    // 종료 
+}					// 함수 끝 
+
---
+++ Test/behappy.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("Be Happy");  // printf( ) : 함수 , " " : 문자열, ' ' : 문자 
+
+	return 0; 
+}
