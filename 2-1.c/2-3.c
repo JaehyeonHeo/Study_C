90cf361eddc560ff9fd5c1d13970da0fe7f2e6c0
JaehyeonHeo <jhheo06@gmail.com>
2021-04-27 12:39:10 +0900

    Commit!

---
+++ 2-1.c/2-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Be happy\n"); // 줄바꿈
+	printf("12345678901234567890\n"); // 줄바꿈
+	printf("My\tfriend\n"); // My 뒤에 탭한후에 줄바꿈 
+	printf("Goot\bd\tchance\n"); // Good에서 백스페이스 후 d 입력, 탭후에 줄바꿈 
+	printf("Cow\rW\a\n"); // 맨앞으로가서 C를 W로 바꾸고 알람소리, 줄바꿈 
+
+
+
+	return 0;
+}
